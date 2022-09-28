b7af67e577e9772a8ef5d26dcabee7217506727e
Efim Shchegolev <efimblashck@yandex.ru>
2022-09-28 20:51:53 +0300

    feat:  добавить функционал login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,54 @@
-function Login() {
+import { useState } from "react";
+
+function Login(props) {
+  const INIT_VALUES = { email: "", password: "", message: "" };
+  const [state, setState] = useState(INIT_VALUES);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((old) => ({
+      ...old,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = state;
+
+    if (!email || !password) return;
+
+    props
+      .onLogin(email, password)
+      .then(() => {
+        setState(INIT_VALUES);
+      })
+      .catch((err) => {
+        console.log(err);
+        setState((old) => ({
+          ...old,
+          message: "Что-то пошло не так!",
+        }));
+      });
+  };
+
   return (
     <div className="auth">
       <h1 className="auth__title">Вход</h1>
       <form className="auth__form">
-        <input className="auth__input auth__input_email" placeholder="Email" />
+        <input
+          className="auth__input auth__input_email"
+          placeholder="Email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+        />
         <input
           className="auth__input auth__input_password"
           placeholder="Пароль"
           type="password"
+          name="password"
+          value={state.value}
+          onChange={handleChange}
         />
         <button className="auth__button">Войти</button>
       </form>
